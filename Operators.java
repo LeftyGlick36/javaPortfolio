b549313cd545536923b9ede7342ea2af2a6605ae
Moses <mopedigg36@gmail.com>
2016-12-13 01:27:35 -0700

    *

--- Operators.java
+++ Operators.java
@@ -2,24 +2,69 @@ package javaPortfolio;
 
 import java.util.Scanner;
 
+/**
+ * Objective In this challenge, you'll work with arithmetic operators. Check out
+ * the Tutorial tab for learning materials and an instructional video!
+ * 
+ * Task Given the meal price (base cost of a meal), tip percent (the percentage
+ * of the meal price being added as tip), and tax percent (the percentage of the
+ * meal price being added as tax) for a meal, find and print the meal's total
+ * cost.
+ * 
+ * Note: Be sure to use precise values for your calculations, or you may end up
+ * with an incorrectly rounded result!
+ * 
+ * Input Format
+ * 
+ * There are lines of numeric input: The first line has a double, (the cost of
+ * the meal before tax and tip). The second line has an integer, (the percentage
+ * of being added as tip). The third line has an integer, (the percentage of
+ * being added as tax).
+ * 
+ * Output Format
+ * 
+ * Print The total meal cost is totalCost dollars., where is the rounded integer
+ * result of the entire bill ( with added tax and tip).
+ * 
+ * Sample Input
+ * 
+ * 12.00 20 8 Sample Output
+ * 
+ * The total meal cost is 15 dollars. Explanation
+ * 
+ * Given: , ,
+ * 
+ * Calculations:
+ * 
+ * 
+ * 
+ * 
+ * We round to the nearest dollar (integer) and then print our result:
+ * 
+ * The total meal cost is 15 dollars.
+ * 
+ * @author mosesmanning
+ *
+ */
 public class Operators {
 	public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double mealCost = scan.nextDouble(); // original meal price
-        int tipPercent = scan.nextInt(); // tip percentage
-        int taxPercent = scan.nextInt(); // tax percentage
-        scan.close();
-      
-        // Write your calculation code here.
-      
-        // cast the result of the rounding operation to an int and save it as totalCost 
-        double tipTotal = (mealCost*tipPercent)/100;
-        double taxTotal = (taxPercent* mealCost)/100;
-        
-        double totalMealCost = tipTotal + taxTotal + mealCost;
-        int totalCost = (int) Math.round(totalMealCost);
-      
-        // Print your result
-        System.out.println("The total meal cost is " + totalCost +  " dollars.");
-    }
+		Scanner scan = new Scanner(System.in);
+		double mealCost = scan.nextDouble(); // original meal price
+		int tipPercent = scan.nextInt(); // tip percentage
+		int taxPercent = scan.nextInt(); // tax percentage
+		scan.close();
+
+		// Write your calculation code here.
+
+		// cast the result of the rounding operation to an int and save it as
+		// totalCost
+		double tipTotal = (mealCost * tipPercent) / 100;
+		double taxTotal = (taxPercent * mealCost) / 100;
+
+		double totalMealCost = tipTotal + taxTotal + mealCost;
+		int totalCost = (int) Math.round(totalMealCost);
+
+		// Print your result
+		System.out.println("The total meal cost is " + totalCost + " dollars.");
+	}
 }
---
+++ Person.java
@@ -0,0 +1,103 @@
+package javaPortfolio;
+
+import java.util.Scanner;
+
+/**
+ * Objective In this challenge, we're going to learn about the difference
+ * between a class and an instance; because this is an Object Oriented concept,
+ * it's only enabled in certain languages. Check out the Tutorial tab for
+ * learning materials and an instructional video!
+ * 
+ * Task Write a Person class with an instance variable, , and a constructor that
+ * takes an integer, , as a parameter. The constructor must assign to after
+ * confirming the argument passed as is not negative; if a negative argument is
+ * passed as , the constructor should set to and print Age is not valid, setting
+ * age to 0.. In addition, you must write the following instance methods:
+ * 
+ * yearPasses() should increase the instance variable by . amIOld() should
+ * perform the following conditional actions: If , print You are young.. If and
+ * , print You are a teenager.. Otherwise, print You are old.. To help you learn
+ * by example and complete this challenge, much of the code is provided for you,
+ * but you'll be writing everything in the future. The code that creates each
+ * instance of your Person class is in the main method. Don't worry if you don't
+ * understand it all quite yet!
+ * 
+ * Note: Do not remove or alter the stub code in the editor.
+ * 
+ * Input Format
+ * 
+ * Input is handled for you by the stub code in the editor.
+ * 
+ * The first line contains an integer, (the number of test cases), and the
+ * subsequent lines each contain an integer denoting the of a Person instance.
+ * 
+ * Constraints
+ * 
+ * Output Format
+ * 
+ * Complete the method definitions provided in the editor so they meet the
+ * specifications outlined above; the code to test your work is already in the
+ * editor. If your methods are implemented correctly, each test case will print
+ * or lines (depending on whether or not a valid was passed to the constructor).
+ * 
+ * @author mosesmanning
+ *
+ */
+public class Person {
+
+	private int age;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+		}
+		sc.close();
+	}
+
+	public Person(int initialAge) {
+		// Add some more code to run some checks on initialAge
+		age = initialAge;
+	}
+
+	public void amIOld() {
+		// Write code determining if this person's age is old and print the
+		// correct statement:
+		String young = "You are young.";
+		String teen = "You are a teenager.";
+		String old = "You are old.";
+		
+		if (!(age > 0)) {
+			age = 0;
+			String zero = "Age is not valid, setting age to " + Person.this.age + ".";
+
+			System.out.println(zero.trim());
+		}
+
+		if (age < 13) {
+			System.out.println(young.trim());
+		}
+
+		if ((age >= 13) && (age < 18)) {
+			System.out.println(teen.trim());
+		}
+
+		if (age >= 18) {
+			System.out.println(old.trim());
+		}
+	}
+
+	public void yearPasses() {
+		// Increment this person's age.
+		this.age++;
+	}
+
+}
